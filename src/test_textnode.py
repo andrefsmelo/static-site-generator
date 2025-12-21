3d9a9e9046a9497c49df8464e1c161797d45b371
andrefsmelo <andreemelo@gmail.com>
2025-12-21 10:40:47 +0000

    feat: implement text_node_to_html_node conversion

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -17,6 +17,35 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.CODE)
         node2 = TextNode("This is a text node", TextType.LINK)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Este é um texto em negrito", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Este é um texto em negrito")
+
+    def test_link(self):
+        node = TextNode("Clique aqui", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Clique aqui")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
+
+    def test_image(self):
+        node = TextNode("Texto alternativo", TextType.IMAGE, "https://www.boot.dev/img.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, 
+            {"src": "https://www.boot.dev/img.png", "alt": "Texto alternativo"}
+        )
     
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -25,3 +26,17 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(tag = None, value = text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode(tag = "b", value = text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode(tag = "i", value = text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode(tag = "code", value = text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode(tag = "a", value = text_node.text, props={"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode(tag = "img", value = "", props={"src": text_node.url, "alt": text_node.text})
